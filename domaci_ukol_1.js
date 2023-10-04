d8151900f73866fa6451fe7218b285da1cc2f762
Matěj Šoffr <140012280+Soffik94@users.noreply.github.com>
2023-10-04 15:36:31 +0200

    Update domaci_ukol_1.js

--- domaci_ukol_1.js
+++ domaci_ukol_1.js
@@ -4,11 +4,11 @@ const decimalInput = -42;
 function decimalToBinary(decimalNumber) {
     // Creating variable for return value of the function
     let binary = '';
-    // Converting decimalNumber to binary if decimalNumber equals 0
+    // Converting decimalInput to binary if decimalInput equals 0
     if (decimalNumber === 0) {
         return '0';
     }
-    // Converting decimalNumber to binary if decimalNumber < 0
+    // Converting decimalInput to binary if decimalInput < 0
     if (decimalNumber < 0){
             decimalNumber = decimalNumber*(-1);
             while (decimalNumber > 0) {
@@ -17,7 +17,7 @@ function decimalToBinary(decimalNumber) {
             }
             return -binary;
     }
-    // Converting decimalNumber to binary if decimalNumber > 0
+    // Converting decimalInput to binary if decimalInput > 0
     while (decimalNumber > 0) {
         binary = (decimalNumber % 2) + binary;
         decimalNumber = Math.floor(decimalNumber / 2);
